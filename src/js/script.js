9ed7ef6e1a233380b00b1be5c9630d928978bb61
Mana1010 <tristanvicclarito2003@gmail.com>
2024-06-01 23:04:59 +0800

    Adding navigation bar and footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,46 @@
+const backToTop = document.querySelector("#back-to-top");
+const form = document.querySelector("form");
+const navLinks = document.querySelectorAll(".nav-link");
+const menu = document.querySelector("#menu");
+const buttonMenu = document.querySelector("#button-menu");
+//This function is use to scroll at the very top of the window
 
+function backToTopFunc() {
+  window.scrollTo(0, 0);
+}
+function submitForm(e) {
+  e.preventDefault();
+  Swal.fire({
+    title: "Good job!",
+    text: "Your Message successfully submitted",
+    icon: "success",
+  });
+}
+
+function handleUrlChange() {
+  const currentUrl = location.href.split("#");
+  const pathname = currentUrl.at(-1);
+  navLinks.forEach((link) => {
+    const currentLink = link.getAttribute("href");
+    if (currentLink === `#${pathname}`) {
+      link.classList.add("bg-[#E56E1E]");
+      console.log(link);
+    } else {
+      link.classList.remove("bg-[#E56E1E]");
+    }
+  });
+}
+function showMainMenu() {
+  if (menu.classList.contains("hidden")) {
+    menu.classList.add("block");
+    menu.classList.remove("hidden");
+  } else {
+    menu.classList.add("hidden");
+    menu.classList.remove("block");
+  }
+}
+handleUrlChange();
+window.addEventListener("popstate", handleUrlChange);
+form.addEventListener("submit", submitForm);
+backToTop.addEventListener("click", backToTopFunc);
+buttonMenu.addEventListener("click", showMainMenu);
